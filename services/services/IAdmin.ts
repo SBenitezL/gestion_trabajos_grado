874481db55d6f3da6d8689532439205f27db7973
Santiago Benitez Lopez <sbenitez1607@gmail.com>
2023-09-23 18:44:57 -0500

    Planeación entrega 29

---
+++ services/services/IAdmin.ts
@@ -0,0 +1,20 @@
+interface IAdmin
+{
+    //200
+    //201 -> Crear
+    //202   -> Actualizar
+    //203 -> Eliminar
+
+    //304
+    crearDirector():any;
+    crearJefatura():any;
+    crearAuxiliarJefatura():any;
+    crearSecretariaConsejo():any;
+    crearEvaluador():any;
+    crearDecano():any;
+    //Para el update todo
+    
+    //Eliminar
+    
+    //Para el read consultar por rol, usuario y todos.
+}
---
+++ services/services/IDirector.ts
@@ -0,0 +1,4 @@
+interface IDirector
+{
+    
+}
