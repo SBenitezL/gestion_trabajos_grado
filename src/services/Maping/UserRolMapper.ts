f951eca33ea8e825d6791e47d0140dc3b6099776
Santiago Benitez Lopez <sbenitez1607@gmail.com>
2023-10-02 18:35:39 -0500

    Fix: Se arregla el mappper para que coincida con los cambios enel modelo de base de datos

---
+++ src/services/Maping/UserRolMapper.ts
@@ -0,0 +1,38 @@
+import UsuarioRolEntity from "../../models/UsuarioRolEntity";
+import UsuarioRolDTO from "../DTO/UsuarioRolDTO";
+
+export default class UsuarioRolMapper{
+    public constructor(){
+
+    }
+    /**
+     * Combierte una lista de UsuarioRolEntity en un objeto UsuarioRolDTO, 
+     * se utiliza cuando los datos corresponden a los de un solo usuario
+     * @param {UsuarioRolEntity} objEntity 
+     * 
+     */
+    public entityToDTO(objEntity:UsuarioRolEntity[]):UsuarioRolDTO{
+        var usuario:UsuarioRolDTO = new UsuarioRolDTO(0,"","","",[]);
+        objEntity.forEach((row)=>{
+            if(usuario.id != row.usr_codigo)
+            {
+                usuario.id = row.usr_codigo;
+                usuario.login = row.usr_login;
+                usuario.nombre = row.usr_nombre;
+                usuario.password = row.usr_password;
+                usuario.rol =[];
+            }
+            usuario.rol.push(new RolDTO(row.rol_id, row.rol_nombre));
+        })
+       return usuario;
+    }
+
+    public dtoToEntity(objDTO:UsuarioRolDTO): UsuarioRolEntity[]
+    {
+        var usuario:UsuarioRolEntity[] = [];
+        objDTO.rol.forEach((row)=>{
+            usuario.push(new UsuarioRolEntity(objDTO.id, objDTO.login,objDTO.nombre, objDTO.password, row.id, row.nombre));
+        })
+        return usuario;
+    }
+}
