bf362358bef5fd7db6b76eeda29bc6f6dc4a1d76
Daniela <driascos@unicauca.edu.co>
2023-10-03 00:50:31 -0500

    Implementacion gestionUsuariosImpl

--- src/services/services/GestionUsuariosImpl.ts
+++ src/services/services/GestionUsuariosImpl.ts
@@ -1,3 +1,4 @@
+import UsuarioRolEntity from '../../models/UsuarioRolEntity';
 import GestionUsuariosRepository from '../../repositories/GestionUsuariosRepository';
 import UsuarioRolDTO from '../DTO/UsuarioRolDTO';
 import UsuarioRolMapper from '../Maping/UserRolMapper';
@@ -13,20 +14,25 @@ export default class GestionUsuariosImpl implements IGestionUsuarios{
         var usuarioEntity = this.mapper.dtoToEntity(usuario);
         throw new Error('Method not implemented.');
     }
-    actualizarUsuario(id: number, usuario: UsuarioRolDTO): UsuarioRolDTO {
-        throw new Error('Method not implemented.');
+    async actualizarUsuario(id: number, usuario: UsuarioRolDTO): Promise<UsuarioRolDTO> {
+        const usuarioEntity:UsuarioRolEntity[]= this.mapper.dtoToEntity(usuario)
+        const res= await this.accesoPersistencia.actualizarUsuario(id,usuarioEntity);
+        return this.mapper.entityToDTO(res);
+        
     }
     eliminarUsuario(id: number): boolean {
         throw new Error('Method not implemented.');
     }
-    consultarUsuarios(): UsuarioRolDTO[] {
-        throw new Error('Method not implemented.');
+    async consultarUsuarios(): Promise<UsuarioRolDTO[]> {
+        const res= await this.accesoPersistencia.consultarUsuarios();
+        //return this.mapper.entitiesToDTOs(res);
     }
     consultarUsuarioPorId(id: number): UsuarioRolDTO {
         throw new Error('Method not implemented.');
     }
-    consultarUsuariosPorRol(rolId: number): UsuarioRolDTO[] {
-        throw new Error('Method not implemented.');
+    async consultarUsuariosPorRol(rolId: number): Promise<UsuarioRolDTO[]>  {
+        const res= await this.accesoPersistencia.consultarUsuariosPorRol(rolId);
+        //return this.mapper.entitiesToDTOs(res);
     }
     consultarUsuariosPorLogin(login: string): UsuarioRolDTO[] {
         throw new Error('Method not implemented.');
