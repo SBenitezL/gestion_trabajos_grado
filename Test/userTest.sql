6f004a869c49559a0a87448d022ffbe30f988b52
Juan Ante <juan09ante@gmail.com>
2023-10-19 10:41:03 -0500

    Create userTest.sql

---
+++ Test/userTest.sql
@@ -0,0 +1,3 @@
+INSERT INTO `gestion_trabajo_grado`.`usuario` (`usr_codigo`, `usr_nombre`, `usr_login`, `usr_password`, `usr_correo`) VALUES ('0', 'prueba', 'user', 'password', 'user@test.com');
+INSERT INTO `gestion_trabajo_grado`.`rol` (`ROL_ID`, `ROL_NOMBRE`) VALUES ('10', 'testRol');
+INSERT INTO `gestion_trabajo_grado`.`usuariorol` (`USR_CODIGO`, `ROL_ID`, `FECHAINICIO`) VALUES ('0', '10', '2023-01-01');
