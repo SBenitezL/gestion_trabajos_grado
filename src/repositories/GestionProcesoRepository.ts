e66bc485edfa223836d44b00d473d3a8955fd8e6
Daniela <driascos@unicauca.edu.co>
2023-10-19 19:17:14 -0500

    Se termina la consulta de un proceso especifico

--- src/repositories/GestionProcesoRepository.ts
+++ src/repositories/GestionProcesoRepository.ts
@@ -18,13 +18,23 @@ export default class GestionProcesoRepository{
     }
     public async consultarProceso(id:number):Promise<ProcesoEntity>
     {
-       const query ="SELECT * FROM PROCESO WHERE PRC_ID = ?";
-        
-        throw new Error("Method not implemented.");   
+       const query ="SELECT PRC_ID, USR_CODIGO, A_ID, B_ID, C_ID, ASE_CC, PRC_FORM_A, PRC_FORM_B, PRC_FORM_C, PRC_TITULO, PRC_TIPO FROM PROCESO WHERE PRC_ID = ?";
+       const res: ProcesoEntity = new ProcesoEntity(0,0,0,0,0,0,false,false,false,"",""); // Crear un objeto vacío
+    
+       try {
+           const [result]: ProcesoEntity[] | any = await db.query(query, [id]);
+   
+           if (result.length > 0) {
+               return result[0]; 
+           }
+   
+           return res; 
+       } catch (error) {
+           throw error; 
+       }
     }
     public async consultarProcesos():Promise<ProcesoListEntity[]>
     {
-        //revisar prc_tipo y la igualdad 
        // const query = "SELECT p.prc_titulo, p.prc_tipo, e.est_nombre FROM proceso p INNER JOIN estudiante e ON p.PRC_ID=e.PRC_ID WHERE p.prc_id =?;";
        const query ="SELECT p.prc_titulo, p.prc_tipo, e.est_nombre FROM proceso p INNER JOIN estudiante e ON p.PRC_ID=e.PRC_ID"; 
        const res: ProcesoListEntity[]=[];
