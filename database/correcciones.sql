068899c43a314a897fb802e3079311d69d5f02d8
Juan Ante <juan09ante@gmail.com>
2023-10-30 17:43:57 -0500

    Update correcciones.sql

--- database/correcciones.sql
+++ database/correcciones.sql
@@ -1,5 +1,7 @@
+-- Esto ya esta en el script
 alter table bandejaentrada add constraint FK_BANDEJAENTRADA_USUARIO foreign key (usr_codigo)
 	references usuario(usr_codigo) on delete restrict on update restrict;
+-- 
 
 alter table proceso drop column inv_codigo;
 ALTER TABLE proceso ADD prc_tipo VARCHAR(30);
