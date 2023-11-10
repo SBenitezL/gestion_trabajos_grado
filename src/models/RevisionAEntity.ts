bde0e89754c5ed99cc0ddd5f7e8bcf9e7cac2dac
Santiago Benitez Lopez <sbenitez1607@gmail.com>
2023-11-09 19:36:42 -0500

    ADD: RevisionAEntity

---
+++ src/models/RevisionAEntity.ts
@@ -0,0 +1,16 @@
+export default class RevisionAEntity
+{
+    constructor(
+        public prc_id:number,
+        public prc_tipo:string,
+        public prc_titulo:string,
+        public est_codigo:string,
+        public est_nombre:string,
+        public fA_estado:number,
+        public fA_revisiones:number
+        )
+    {
+    }
+
+
+}
