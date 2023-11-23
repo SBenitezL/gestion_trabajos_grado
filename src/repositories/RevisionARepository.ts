0ee4722046011fcff09f82ff22f65b90d6d49b60
Daniela <driascos@unicauca.edu.co>
2023-11-22 20:57:33 -0500

    Add procedures and change state of FormA

--- src/repositories/RevisionARepository.ts
+++ src/repositories/RevisionARepository.ts
@@ -21,6 +21,27 @@ class RevisionARepository implements IRevisionARepository
         }
         return res;
     }
+    public async cambiarEstadoFormatoA(codigo:number): Promise<RevisionAEntity[]> {
+        const query = "call cambiarEstadoFormatoA(?)";
+        const query2="call consultarEstadoFormA(?)"
+        const res:RevisionAEntity[] = []
+        try{
+            const [result]:RevisionAEntity|any = await db.query(query,[codigo]);
+            if(result){
+                const [result2]:RevisionAEntity|any = await db.query(query2,[codigo]);
+
+                result2[0].map((row:RevisionAEntity)=>{
+                    res.push(row);
+                })
+            }
+            
+            console.log(result);
+        }catch(error)
+        {
+        }
+        return res;
+    }
+
 }
 const datos = new RevisionARepository();
 export default datos;
