96d76497df4d94c0d9e6bf2a5db670b3f148747e
Daniela <driascos@unicauca.edu.co>
2023-11-22 21:09:44 -0500

    Add route to change state

--- src/controllers/RevisionAController.ts
+++ src/controllers/RevisionAController.ts
@@ -20,6 +20,22 @@ class RevisionAController{
             res.status(500).json({error: "Error en el servidor"});
         }
     }
+
+    public changeState = async(req:Request, res:Response)=>{
+        try
+        {
+            const result = await this.revisionAServices.cambiarEstadoFormatoA(parseInt(req.params.cod));
+            if(result.length > 0)
+            {
+                res.status(200).json(result);
+            }else{
+                res.status(400).json({error:"No se pudo cambiar el estado del formato A"});
+            }
+        }catch(error)
+        {
+            res.status(500).json({error: "Error en el servidor"});
+        }
+    }
 }
 const revisionAController = new RevisionAController(revisionAImpl);
 export default revisionAController;
