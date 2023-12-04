3a8d29fdb0a440953fa5f6bfd71514f43d732a29
Santiago Benitez Lopez <sbenitez1607@gmail.com>
2023-12-04 14:12:10 -0500

    Add: rol coordinacioN

--- database/inserts.sql
+++ database/inserts.sql
@@ -3,6 +3,7 @@ insert into usuario values (104619021331,"santiago benitez",'sbenitez', 'prueba'
 insert into usuariorol values (104619021331,2,sysdate,null);
 INSERT INTO `estudiante` (`EST_CODIGO`, `PRC_ID`, `EST_NOMBRE`, `EST_CORREO`) VALUES ('1', null, 'Santiago', 'sbenitez@unicauca.edu.co');
 INSERT INTO ROL VALUES(3, 'JEFATURA'),(4, 'CONSEJO DE FACULTAD'), (5, 'EVALUADOR');
+INSERT INTO ROL VALUES(6, 'COORDINACIÓN');
 /**
  * SELECT a.ANX_SRC AS RUTA
  * FROM ANEXOS a INNER JOIN PROCESO p
