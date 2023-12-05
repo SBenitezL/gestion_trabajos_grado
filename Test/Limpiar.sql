56d71e161755978064741b4c4c53bfac79cfb1e2
Juan Ante <juan09ante@gmail.com>
2023-12-05 06:27:13 -0500

    cleaning

--- Test/Limpiar.sql
+++ Test/Limpiar.sql
@@ -1,4 +1,8 @@
 update estudiante set prc_id = null;
-delete from proceso;
 delete from archivos;
+delete from evaluarfacultad;
+delete from anexos;
+delete from b_proceso;
+delete from proceso;
 delete from ti_a;
+delete from ti_b;
